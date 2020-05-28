2f71c56ccd3e5532fc3b9c82813442eb19b2ab6c
Kaleab G <kaleabsolomon470@gmail.com>
2020-05-28 11:59:51 -0500

    Quiz semi-functional

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,41 @@
 const SPOTIFY_LINK = "https://open.spotify.com/playlist/6rmiBWfdA4jGdTWaYMAQri";
 
 
-/**
- * Sends user a spotify link
- */
+
+/* Sends user a spotify link.*/
 function sendSpotifyLink(){
     const linkContainer = document.getElementById("spotify-link-container");
     linkContainer.innerHTML = "<em>Jazz isn't an avenue I've explored!</em> Here is the spotify link: \n" + SPOTIFY_LINK;
 }
 
+// document.getElementById("quiz").addEventListener("submit", function(event) {
+//   const data = new FormData(form);
+//   let output = "";
+//   for (const entry of data) {
+//     output = output + entry[0] + "=" + entry[1] + "\r";
+//   }
+
+//   console.log(output);
+// });
+
+let clickedItems = [];
+
+document.getElementById("quiz").addEventListener("click", function(event){
+    clickedItems.push(event.srcElement.id);
+    generateNextQuestion();
+
+});
+
+let question2 = {
+    question: "Which soccer team do I support?",
+    choice1: "Manchester City",
+    choice2: "Liverpool"
+}
+
+function generateNextQuestion(){
+    document.getElementsByClassName("question")[0].innerText = question2.question;
+
+}
 
 /**
  * Adds a random greeting to the page.
