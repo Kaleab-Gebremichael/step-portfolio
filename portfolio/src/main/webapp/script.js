649dadd5ee47a905cab42b051cd8c072cc284a7a
Kaleab G <kaleabsolomon470@gmail.com>
2020-05-28 15:27:09 -0500

    Edits based on PR comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,16 +14,39 @@
 
 const SPOTIFY_LINK = "https://open.spotify.com/playlist/6rmiBWfdA4jGdTWaYMAQri";
 
+const RIGHT_ANSWERS = ["choice-2", "choice-2", "choice-1"];
+
+const MOVIES =
+      ['Austin Powers', 'Ace Ventura', 'Talladega Nights', 'The dictator'];
+
+const QUESTION_2 = {
+    question: "Which soccer team do I support?",
+    choice1: "Manchester City",
+    choice2: "Liverpool"
+}
+
+const QUESTION_3 = {
+    question: "Which of the following apps don't I have on my phone?",
+    choice1: "Tiktok",
+    choice2: "Reddit"
+}
+
+const QUESTIONS_LIST = [QUESTION_2,QUESTION_3];
 
 let clickedItems = [];
-let rightAnswers = ["choice-2", "choice-2", "choice-1"];
 let currentQuestionNumber = 0;
 
+/** 
+ *  This function records the choices of the user and makes call to next 
+ *  question generator if appropriate. 
+*/
 document.getElementById("quiz").addEventListener("click", function(event){
     document.getElementById("quiz").reset();
-    if(currentQuestionNumber != questionList.length){
+
+    if(currentQuestionNumber != QUESTIONS_LIST.length){
         clickedItems.push(event.srcElement.id);
         generateNextQuestion(currentQuestionNumber++);
+    
     } else {
         document.getElementById("quiz").classList.add("hidden");
 
@@ -33,37 +56,29 @@ document.getElementById("quiz").addEventListener("click", function(event){
 
 });
 
-let question2 = {
-    question: "Which soccer team do I support?",
-    choice1: "Manchester City",
-    choice2: "Liverpool"
-}
-
-let question3 = {
-    question: "Which of the following apps don't I have on my phone?",
-    choice1: "Tiktok",
-    choice2: "Reddit"
-}
-
-const questionList = [question2,question3];
-
+/**
+    This function displays the next question available.
+    @type {number} questionNumber - the question to display
+ */
 function generateNextQuestion(questionNumber){
-    document.getElementsByClassName("question")[0].innerText = questionList[questionNumber].question;
-    document.getElementsByClassName("choice-1")[0].innerText = questionList[questionNumber].choice1;
-    document.getElementsByClassName("choice-2")[0].innerText = questionList[questionNumber].choice2;
+    document.getElementsByClassName("question")[0].innerText = QUESTIONS_LIST[questionNumber].question;
+    document.getElementsByClassName("choice-1")[0].innerText = QUESTIONS_LIST[questionNumber].choice1;
+    document.getElementsByClassName("choice-2")[0].innerText = QUESTIONS_LIST[questionNumber].choice2;
 }
 
+/** This function calculates and displays the results from the quiz. */
 document.getElementById("button").addEventListener("click",function(){
     document.getElementById("button").classList.add("hidden");
+    
     let score = 0;
-    for(let i = 0; i < rightAnswers.length; ++i){
-        if(clickedItems[i] == rightAnswers[i]){
+    for(let i = 0; i < RIGHT_ANSWERS.length; ++i){
+        if(clickedItems[i] == RIGHT_ANSWERS[i]){
             ++score;
         }
     }
 
     const scoreContainer = document.getElementById("score-container");
-    if(score > questionList.length/2){
+    if(score > QUESTIONS_LIST.length/2){
         scoreContainer.innerHTML = "Your score is " + score + 
             "! Here is the spotify link for you to enjoy: \n" + SPOTIFY_LINK;
     } else {
@@ -73,13 +88,9 @@ document.getElementById("button").addEventListener("click",function(){
 
 });
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomMovie() {
-  const movies =
-      ['Austin Powers', 'Ace Ventura', 'Talladega Nights', 'The dictator'];
 
+/** Adds a random greeting to the page. */
+function addRandomMovie() {
   // Pick a random greeting.
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
