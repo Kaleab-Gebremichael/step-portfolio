0e873d5d3da913dcf7e0ddac6ffced576019e950
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-01 16:58:21 -0500

    Add comma between each item

--- portfolio/src/main/webapp/discussionScript.js
+++ portfolio/src/main/webapp/discussionScript.js
@@ -3,6 +3,6 @@ function displayList(){
   fetch('/discussion')
   .then((response) => response.json())
   .then((data) => {
-    document.getElementById('discussion-container').textContent = data;
+    document.getElementById('discussion-container').textContent = data.join(", ");
   })
 }
