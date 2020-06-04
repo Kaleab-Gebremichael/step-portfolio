3c4ff8769b3107dc94f6da5e3b82d33a1ba11b46
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-04 18:21:26 +0000

    Set up reply interface

--- portfolio/src/main/webapp/discussionScript.js
+++ portfolio/src/main/webapp/discussionScript.js
@@ -13,16 +13,48 @@ function displayList() {
       content.classList.add("post-content");
       content.textContent = data[i].content;
 
-
       let singlePost = document.createElement('div');
       singlePost.classList.add("post");
+      singlePost.id = uniqueIdentifierGenerator();  //as a unique identifier to the post.
+
+      let replyButton = document.createElement('button');
+      replyButton.textContent = "Reply to this post";
+      replyButton.onclick = `createReplyForm(${singlePost.id})`;
+
 
       singlePost.appendChild(title);
       singlePost.appendChild(content);
+      singlePost.appendChild(replyButton);
 
       posts.appendChild(singlePost);
     }
 
     document.getElementById('discussion-container').appendChild(posts);
   })
+}
+
+function createReplyForm(postId){
+
+  let replyForm = document.createElement("form");
+  replyForm.action = `/reply?postID=${postId}`;
+  replyForm.method = "POST";
+
+  let inputBox = document.createElement("textarea");
+  inputBox.rows = "2";
+  inputBox.cols = "40";
+
+  let submitButton = document.createElement("input");
+  submitButton.type = "submit";
+  submitButton.value = "Post";
+
+  replyForm.appendChild(inputBox);
+  replyForm.appendChild(submitButton);
+
+  document.getElementById(postId).appendChild(replyForm);
+}
+
+function uniqueIdentifierGenerator(){
+   const identifier = Math.floor(Math.random() * 1000) + 1;
+
+   return identifier;
 }
