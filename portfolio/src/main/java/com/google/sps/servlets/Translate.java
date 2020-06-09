6c300de72380d9b420370452ee16d0fd077757b1
Kaleab G <kaleabsolomon470@gmail.com>
2020-06-09 20:49:04 +0000

    Change to new project and add translate route

---
+++ portfolio/src/main/java/com/google/sps/servlets/Translate.java
@@ -0,0 +1,28 @@
+
+package com.google.sps.servlets;
+
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/translate")
+public class TranslationServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String languageCode = request.getParameter("language");
+  
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(originalText, Translate.TranslateOption.targetLanguage(languageCode));
+    String translatedText = translation.getTranslatedText();
+
+    response.setContentType("text/html; charset=UTF-8");
+    response.getWriter().println(translatedText);
+  }
+}
